6a574e6425e173212e439bc110a068961331dfca
ddalfovo <davide.dalfovo@unitn.it>
2024-02-12 13:44:18 +0100

    update

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 NBS2 algorithm for TCGA network propagation
 
 Original publication https://github.com/wzhang1984/NBSS
+
+## Pipeline
+### TODO
---
+++ preprocessing_net.R
@@ -0,0 +1,23 @@
+library(RCX)
+library(parallel)
+library(data.table)
+library(igraph)
+setwd("/shares/CIBIO-Storage/BCG/scratch1/ddalfovo/Tools/NBS2_NetPropagation")
+
+### Preparation folder
+dir.create("./data/",showWarnings = F)
+dir.create("./data/somaticAlterations/",showWarnings = F)
+dir.create("./ppi_networks/",showWarnings = F)
+
+### Open the cx network from the Ideker paper about PCNet
+rcx = readCX("./ppi_networks/Parsimonious Composite Network (PCNet).cx")
+# and convert into igraph
+ig <- toIgraph(rcx)
+save(ig,file="data/PCNet_igraph.RData")
+
+### Filter networks based on genes present in PathwayCommons db and save the list of common genes
+all_PC = fread("./data/PathwayCommons11.All.hgnc.txt")
+
+genes_PC = unique(c(all_PC$PARTICIPANT_A,all_PC$PARTICIPANT_B))
+commonGenes_PCnet = data.frame(gene = intersect(genes_PC,V(ig)$nodeName))
+fwrite(commonGenes_PCnet,"./data/commonGenesPCnet.tsv",sep="\t")
